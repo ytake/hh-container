16b1170f68bb0dc89646886ddade627043695cf0
ytake <yuuki.takezawa@comnect.jp.net>
2018-10-08 01:41:53 +0900

    fixed

--- .travis.sh
+++ .travis.sh
@@ -4,6 +4,6 @@ hhvm --version
 curl https://getcomposer.org/installer | hhvm -d hhvm.jit=0 --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
 cd /var/source
-hhvm -d hhvm.php7.all=1 -d hhvm.jit=0 -d hhvm.hack.lang.auto_typecheck=0 /usr/local/bin/composer install
+hhvm -d hhvm.jit=0 -d hhvm.hack.lang.auto_typecheck=0 /usr/local/bin/composer install
 
-hhvm -d hhvm.php7.all=1 -d hhvm.jit=0 -d hhvm.hack.lang.auto_typecheck=0 vendor/bin/phpunit
+hhvm -d hhvm.jit=0 -d hhvm.hack.lang.auto_typecheck=0 vendor/bin/hacktest tests/
